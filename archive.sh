0ddce5fcc1c49d245c04b627f227767c4db5a4ee
mujab <mujababdelmawla@gmail.com>
2026-03-27 02:02:33 +0000

    ..

---
+++ archive.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+
+# define folder and file names 
+
+folder_name="/mnt/c/Users/asus/bash/my_folder"
+file1="$folder_name/file1.txt"
+file2="$folder_name/file2.txt"
+archive_name="/mnt/c/Users/asus/bash/my_folder_archive.tar.gz"
+
+# step1: create the folder 
+mkdir -p "$folder_name"
+
+# step2: create file1.txt and add a content inside it 
+echo "this is the 1st content of the 1st file" > "$file1"
+
+# step3: create file2.txt and copy the content of file1.txt to it 
+cp "$file1" "$file2"
+
+# step4: compress the folder into a .tar.gz
+
+tar -czf "$archive_name" -C "/mnt/c/Users/asus/bash" "my_folder"
